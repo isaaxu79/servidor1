9ce75b35f5eafc8fcfb2cca9fc101f88aab413ed
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-01-25 09:37:23 -0600

    fis..

--- servidor.js
+++ servidor.js
@@ -1,4 +1,4 @@
-const express = require('express')
+//const express = require('express')
 // var aplicacion = express()
 
 // const net = require('net')
@@ -25,13 +25,20 @@ const express = require('express')
 // })
 
 // ser.listen(PORT, HOST)
-server = express()
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  const wss = new SocketServer({ server });
+const wss = new SocketServer({ server });
 
-  wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
-  });
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
