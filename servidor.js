d305b9ecb27a3eb412656a18a935438f2a9377f3
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-01-25 08:44:36 -0600

    first commit

---
+++ servidor.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var aplicacion = express()
+
+const net = require('net')
+const server = require('http').Server(aplicacion)
+const socket = require('socket.io')(server)
+const {StringDecoder} =  require('string_decoder')
+const decoder = new StringDecoder('utf8')
+
+var HOST = "192.168.137.102"
+var PORT = 8081
+
+  server.listen(PORT, function(){
+      console.log('servidor activo ' + HOST + ':' + PORT)
+  })
+
+var ser = net.createServer(function(so){
+    console.log('Usuario Nuevo ' + so.remoteAddress + ':' + so.remotePort)    
+    so.on('data', function(data){
+        console.log(decoder.write(data))
+    })
+    so.on('close', function(){
+      
+    })
+})
+
+ser.listen(PORT, HOST)
