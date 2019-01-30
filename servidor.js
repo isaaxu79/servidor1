c6b857ea40f1c771ac841be533664cb0ef0f3aed
Isaac A. Marin <VasMar_ia@hotmail.com>
2019-01-30 00:34:22 -0600

    built-1

--- servidor.js
+++ servidor.js
@@ -12,7 +12,7 @@ for(var k in interface){
   for(var k2 in interface[k]){
     var address = interface[k][k2]
     if(address.family = "IPv4" && !address.internal){
-      ipDimanic = address.address
+      ipDimanic = address.address.toString()
     }
   }
 }
